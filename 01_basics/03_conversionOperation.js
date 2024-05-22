c69b73cc70f49994173f39920de21971dc33e244
Vishal Nalawade <130028407+vishalnalawade11@users.noreply.github.com>
2024-05-22 10:49:54 +0000

    Updated

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let scope = null // 33, abc, 
+
+console.log(typeof scope);
+
+let value = Number(scope)
+
+console.log(typeof value);
+
+console.log(value);
+
+let isBoolean = 1
+
+console.log(Boolean(isBoolean));
+
+let someNumber = 99
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum);
