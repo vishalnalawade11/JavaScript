b002aaa2ac9be2ad3b0b7228d091ae05c87e9474
Vishal Nalawade <130028407+vishalnalawade11@users.noreply.github.com>
2024-05-22 08:02:10 +0000

    committed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountNumber = 12345
+let accountEmail = "vishal@gmail.com"
+var accountPassword = "54321"
+
+//accountNumber = 55664
+accountEmail = "xyz@gmail.com"
+accountPassword = "78945"
+
+console.log(accountNumber);
+console.table([accountEmail, accountPassword])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+console.log(typeof "vishal")
+console.log(typeof 20)
+console.log(typeof null) // typeof null is object
+console.log(typeof undefined)
+
+// Number
+// string
+// big Int
+// Symbol
+// Boolean
+// null
+// undefined
+
